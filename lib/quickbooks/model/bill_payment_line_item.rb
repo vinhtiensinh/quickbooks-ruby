806d92810a90de2d2adf99024150974f9fdc0e2a
Vinh Tran <qvinh.tran@gmail.com>
2020-04-29 23:03:07 +1000

    add deposit so that we can sync

--- lib/quickbooks/model/bill_payment_line_item.rb
+++ lib/quickbooks/model/bill_payment_line_item.rb
@@ -23,6 +23,11 @@ module Quickbooks
       def vendor_credit_id=(id)
         update_linked_transactions([id], 'VendorCredit')
       end
+
+      def deposit_id=(id)
+        update_linked_transactions([id], 'Deposit')
+      end
+
       alias_method :bill_ids=, :bill_id=
       alias_method :vendor_credit_ids=, :vendor_credit_id=
 
--- lib/quickbooks/version.rb
+++ lib/quickbooks/version.rb
@@ -1,3 +1,3 @@
 module Quickbooks
-  VERSION = "1.0.7"
+  VERSION = "1.0.8"
 end
